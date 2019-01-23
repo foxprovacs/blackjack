542b559465af3910e499d7e86f5a5af8d7903d70
foxprovacs <foxprovacs@gmail.com>
2019-01-22 20:25:39 -0600

    moved player classes to separate file

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 from abc import ABC, abstractmethod
+import blackjack_players
 
 
 class Card:
@@ -42,7 +43,7 @@ class Game:
 	def __init__(self, number_of_decks=8):
 		self.cards = []
 		self.players = {}
-		self.dealer = Dealer()
+		self.dealer = blackjack_players.Dealer()
 				
 		for i in range(1,number_of_decks+1):
 			self.cards = self.cards + Deck().cards
@@ -98,76 +99,10 @@ class Game:
 		""" Players cannot see the first card drawn to the dealer """
 		return self.dealer.hand[1].get_value()
 
-		
-class Player(ABC):
-
-	def __init__(self, name=''):
-		self.hand = []
-		self.name = name
-		self.game = None
-		self.is_winner = False
-		
-	def add_card(self, card):
-		self.hand.append(card)
-		
-	def current_hand_value(self):
-		# Have Aces go last; easier to account for 11 or 1 values
-		sorted_cards = sorted(self.hand, key=lambda x: x.rank_order, reverse=False)
-	
-		value = 0
-		for card in sorted_cards:
-			new_value = value + card.get_value()
-			if new_value > 21 and card.value == 'A':
-				new_value = value + card.get_value(ace_is_low=True)
-			value = new_value
-		return value
-		
-	def is_bust(self):
-		return self.current_hand_value() > 21
-	
-	@abstractmethod
-	def should_hit(self):
-		pass
-		
-	def __repr__(self):
-		return self.name
-
-
-class Dealer(Player):
-
-	def __init__(self):
-		super(Dealer, self).__init__()
-		self.upcard = None
-
-	def should_hit(self):
-		return self.current_hand_value() <= 16
-		
-
-class BasicPlayer(Player):
-
-	def should_hit(self):
-		return self.current_hand_value() <= 10
-		
-
-class SmartPlayer(Player):
-	def should_hit(self):
-		if self.current_hand_value() >= 12 and self.game.dealer_showing() < 10:
-			return False
-		else:
-			return True
-			
-
-class RealPlayer(Player):
-
-	def should_hit(self):
-		x = input('(H)it or (S)tay? ')
-		return x.upper() == 'H'
-			
-		
 
 if __name__ == '__main__':
 	game = Game()
-	game.add_player(RealPlayer('A'))
+	game.add_player(blackjack_players.RealPlayer('A'))
 	
 	for i in range(1,2):
 		print('Round ' + str(i))
@@ -176,9 +111,7 @@ if __name__ == '__main__':
 		#game.clear_hand()
 		print('------------------------')
 	
-	# game.players['A'].add_card(Card('A','D'))
-	# game.players['A'].add_card(Card('Q','C'))
-	# game.players['A'].add_card(Card('9','C'))
+
 
 
 
